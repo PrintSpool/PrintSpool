deb451f4eb7f1fd4cac6d37cd2342a8f3c7f3f4b
Rob Gilson <thatotherdude@gmail.com>
2017-11-22 00:46:43 -0500

    feature: load driver initial state from config

--- packages/tegh-driver-serial-gcode/src/reducer.js
+++ packages/tegh-driver-serial-gcode/src/reducer.js
@@ -1,64 +1,78 @@
-const serialGCodeReducer = (config) => {
-  const initialState = {
-    targetTemperaturesCountdown: null
-    heaters: config.heaters.reduce(), // TODO
-    fans: {
-    }
-    ready: false,
-    error: null,
-  }
+const initializeCollection = (arrayOfIDs, initialValueFn) => {
+  return arrayOfIDs.reduce(
+    ((collection, id) => collection[id] = initialValueFn()),
+    {}
+  )
+}
 
-  return (state = initialState, action) => {
-    switch(action.type) {
-      case 'SERIAL_RECEIVE':
-        if (action.parsedData.temperatures != null) {
-          const heaters = {...heaters}
-          action.parsedData.temperatures.forEach((k, v) => {
-            heaters[k] = {...heaters[k], currentTemperature: v}
-          })
-          const {targetTemperaturesCountdown} = action.parsedData
-          return {
-            ...state,
-            targetTemperaturesCountdown,
-            heaters,
-          }
-        }
-        if (action.parsedData.isError) {
-          return {
-            ...state
-            ready: false,
-            error: action.data
-          }
-        }
-        return state
-      case 'SERIAL_SEND':
-        const {parsedData} = action
-        const {type} = parsedData
-        if (type == null) return state
-        const collectionID = {
-          'HEATER_CONTROL': 'heaters',
-          'FAN_CONTROL': 'fans',
-        }[type]
-        if (collectionID == null) throw new Error(`Invalid type: ${type}`)
-        // update the heater or fan's state.
+const initialState = (config) => ({
+  targetTemperaturesCountdown: null
+  heaters: initializeCollection(config.heaters, () => ({
+    currentTemperature: 0,
+    targetTemperature: 0,
+    blocking: false,
+  }),
+  fans: initializeCollection(config.fans, () => ({
+    enabled: false,
+    speed: 0,
+  }),
+  ready: false,
+  error: null,
+})
+
+const serialGCodeReducer = (config) => (
+  state = initialState(config),
+  action
+) => {
+  switch(action.type) {
+    case 'SERIAL_RECEIVE':
+      if (action.parsedData.temperatures != null) {
+        const heaters = {...heaters}
+        action.parsedData.temperatures.forEach((k, v) => {
+          heaters[k] = {...heaters[k], currentTemperature: v}
+        })
+        const {targetTemperaturesCountdown} = action.parsedData
         return {
           ...state,
-          [collectionID]: {
-            ...state[collectionID],
-            [parsedData.id]: {
-              ...state[collectionID][parsedData.id],
-              ..._.without(parsedData, ['id', 'type']),
-            }
+          targetTemperaturesCountdown,
+          heaters,
+        }
+      }
+      if (action.parsedData.isError) {
+        return {
+          ...state
+          ready: false,
+          error: action.data
+        }
+      }
+      return state
+    case 'SERIAL_SEND':
+      const {parsedData} = action
+      const {type} = parsedData
+      if (type == null) return state
+      const collectionID = {
+        'HEATER_CONTROL': 'heaters',
+        'FAN_CONTROL': 'fans',
+      }[type]
+      if (collectionID == null) throw new Error(`Invalid type: ${type}`)
+      // update the heater or fan's state.
+      return {
+        ...state,
+        [collectionID]: {
+          ...state[collectionID],
+          [parsedData.id]: {
+            ...state[collectionID][parsedData.id],
+            ..._.without(parsedData, ['id', 'type']),
           }
         }
-      case 'PRINTER_READY':
-        return {
-          ...state,
-          ready: true,
-        }
-      default:
-        return state
-    }
+      }
+    case 'PRINTER_READY':
+      return {
+        ...state,
+        ready: true,
+      }
+    default:
+      return state
   }
 }
 
