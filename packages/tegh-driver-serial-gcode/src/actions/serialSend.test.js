973a9ac26a08c9bc2bc2047d6ab5b83127e5af87
D1plo1d <thatotherdude@gmail.com>
2018-01-24 16:45:05 -0500

    fixing serial send test

--- packages/tegh-driver-serial-gcode/src/actions/serialSend.test.js
+++ packages/tegh-driver-serial-gcode/src/actions/serialSend.test.js
@@ -2,15 +2,17 @@
 import serialSend from './serialSend'
 
 test('adds checksum and line number', () => {
-  const line = '(╯°□°）╯︵ ┻━┻'
+  const line = 'G12345 (╯°□°）╯︵ ┻━┻'
   const lineNumber = 1995
   // See http://reprap.org/wiki/G-code#.2A:_Checksum
-  const expectedOutputLine = 'N1995 (╯°□°）╯︵ ┻━┻*254\n'
+  const expectedOutputLine = `N1995 ${line}*168\n`
 
   const result = serialSend(line, { lineNumber })
 
   expect(result).toEqual({
-    type: "SERIAL_SEND",
+    type: 'SERIAL_SEND',
+    code: 'G12345',
+    lineNumber,
     data: expectedOutputLine,
   })
 })
