a6dcd8186f234c1cf2ac7faae80f7bc51eb1bfed
D1plo1d <thatotherdude@gmail.com>
2018-01-24 16:46:40 -0500

    fixing serial send test

--- packages/tegh-driver-serial-gcode/src/actions/serialSend.test.js
+++ packages/tegh-driver-serial-gcode/src/actions/serialSend.test.js
@@ -1,18 +1,34 @@
 // @flow
 import serialSend from './serialSend'
 
-test('adds checksum and line number', () => {
-  const line = 'G12345 (╯°□°）╯︵ ┻━┻'
-  const lineNumber = 1995
-  // See http://reprap.org/wiki/G-code#.2A:_Checksum
-  const expectedOutputLine = `N1995 ${line}*168\n`
+describe('with a line number', () => {
+  test('adds the checksum and line number', () => {
+    const line = 'G12345 (╯°□°）╯︵ ┻━┻'
+    const lineNumber = 1995
+    // See http://reprap.org/wiki/G-code#.2A:_Checksum
+    const expectedOutputLine = `N1995 ${line}*168\n`
 
-  const result = serialSend(line, { lineNumber })
+    const result = serialSend(line, { lineNumber })
 
-  expect(result).toEqual({
-    type: 'SERIAL_SEND',
-    code: 'G12345',
-    lineNumber,
-    data: expectedOutputLine,
+    expect(result).toEqual({
+      type: 'SERIAL_SEND',
+      code: 'G12345',
+      lineNumber,
+      data: expectedOutputLine,
+    })
+  })
+})
+
+describe('with lineNumber: false', () => {
+  test('sends the line without a checksum or line number', () => {
+    const line = 'G12345 (╯°□°）╯︵ ┻━┻'
+    const result = serialSend(line, { lineNumber: false })
+
+    expect(result).toEqual({
+      type: 'SERIAL_SEND',
+      code: 'G12345',
+      lineNumber: false,
+      data: line,
+    })
   })
 })
