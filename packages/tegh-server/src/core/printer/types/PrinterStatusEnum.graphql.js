85f566fd39d8f5cd239074e8e3775db2670f21ad
D1plo1d <thatotherdude@gmail.com>
2018-03-16 18:06:10 -0400

    fixing PrinterStatusEnum

--- packages/tegh-server/src/core/printer/types/PrinterStatusEnum.graphql.js
+++ packages/tegh-server/src/core/printer/types/PrinterStatusEnum.graphql.js
@@ -3,37 +3,32 @@ import {
 } from 'graphql'
 
 const PrinterStatusEnum = new GraphQLEnumType({
-  name: 'PrinterStatus',
+  name: 'PrinterStatusEnum',
   values: {
     CONNECTING: {
-      value: 0,
       description: `
         The printer is being initialized. Attempting to spool anything will
         result in an error.
       `,
     },
     READY: {
-      value: 1,
       description: `
         The printer is connected and may be able to spool tasks/jobs depending
         on the state of \`printer.isIdle\`.
       `,
     },
     DISCONNECTED: {
-      value: 3,
       description: `
         The printer is disconnected or turned off.
       `,
     },
     ERRORED: {
-      value: 2,
       description: `
         The printer is being initialized. Attempting to spool anything will
         result in an error.
       `,
     },
     ESTOPPED: {
-      value: 3,
       description: `
         The printer is estopped. Attempting to spool anything except for an
         emergency macro (ie. reset) will result in an error. Spool the \`reset\`
