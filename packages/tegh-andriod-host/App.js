0578198566065d9bccb647838d6d5a2bca4a40b3
D1plo1d <thatotherdude@gmail.com>
2018-08-19 00:51:51 -0700

    fixing nohoist and adding usb serial hello world

--- packages/tegh-andriod-host/App.js
+++ packages/tegh-andriod-host/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import { UsbSerial} from 'react-native-usbserial'
+
+const usbs = new UsbSerial();
+
+async function getDeviceAsync() {
+
+    try {
+        const deviceList = await usbs.getDeviceListAsync();
+        const firstDevice = deviceList[0];
+
+        console.log(firstDevice);
+
+        if (firstDevice) {
+            const usbSerialDevice = await usbs.openDeviceAsync(firstDevice);
+
+            console.log(usbSerialDevice);
+        }
+    } catch (err) {
+        console.warn(err);
+    }
+}
 
 export default class App extends React.Component {
+  componentDidMount() {
+    getDeviceAsync()
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app! Test123</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Shake your phone to open the developer menuwaaat.</Text>
       </View>
     );
   }
