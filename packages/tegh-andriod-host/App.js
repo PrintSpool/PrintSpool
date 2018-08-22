7f31abdc5aa3518f4caa847017bb3e93c610bbb0
D1plo1d <thatotherdude@gmail.com>
2018-08-22 09:28:03 -0700

    linking tegh-core

--- packages/tegh-andriod-host/App.js
+++ packages/tegh-andriod-host/App.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-// import { SerialPort } from 'tegh-core'
+import { SerialPort } from 'tegh-core'
 
 // import { UsbSerial} from 'react-native-usbserial'
 //
 // const usbs = new UsbSerial()
 
 console.log('wat')
-// const usbList = SerialPort.getDeviceList()
+const usbList = SerialPort.getDeviceList()
 //
-// usbList.then(result => console.log(result))
+usbList.then(result => console.log(result))
 
 export default class App extends React.Component {
-  componentDidMount() {
-    getDeviceAsync(this)
-  }
+  // componentDidMount() {
+  //   getDeviceAsync(this)
+  // }
 
   render() {
     return (
