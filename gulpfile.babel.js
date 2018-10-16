8ed3c87569276978e597e668d9145fe1e369ac30
D1plo1d <thatotherdude@gmail.com>
2018-10-16 02:46:10 -0400

    Fixing gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,10 @@
-// import chalk from 'chalk'
+import chalk from 'chalk'
 import rimraf from 'rimraf'
 import path from 'path'
 import gulp from 'gulp'
 import gulpWatch from 'gulp-watch'
 import babel from 'gulp-babel'
-// import through from 'through2'
+import through from 'through2'
 import plumber from 'gulp-plumber'
 import gutil from 'gulp-util'
 
@@ -13,12 +13,12 @@ import babelConfig from './.babelrc'
 
 const { packages } = packageJSON.workspaces
 
-// const compilationLogger = () => (
-//   through.obj((file, enc, callback) => {
-//     gutil.log(`Compiling '${chalk.cyan(file.relative)}'...`)
-//     callback(null, file)
-//   })
-// )
+const compilationLogger = () => (
+  through.obj((file, enc, callback) => {
+    gutil.log(`Compiling '${chalk.cyan(file.relative)}'...`)
+    callback(null, file)
+  })
+)
 
 const errorsLogger = () => (
   plumber({
@@ -28,33 +28,49 @@ const errorsLogger = () => (
   })
 )
 
-const clean = (pkgLocation, done) => {
-  // gutil.log(`Cleaning ${chalk.cyan(pkg.name())}`)
-
-  rimraf(path.join(pkgLocation, '/dist'), done)
+const clean = (done) => {
+  rimraf(path.join(__dirname, 'packages/*/dist'), done)
 }
 
-const getGlobForPackage = location => (
-  path.join(location, '/src/**/*.js')
+const srcFiles = `packages/@(${packages.join('|').replace(/packages\//g, '')})/src/**/*.js`
+
+const rename = fn => (
+  through.obj((file, enc, callback) => {
+    // eslint-disable-next-line no-param-reassign
+    file.path = fn(file)
+    callback(null, file)
+  })
 )
 
-const build = pkgLocation => (
-  // gutil.log(`Building '${chalk.cyan(pkg.name())}'`)
-  gulp
-    .src(getGlobForPackage(pkgLocation))
+const buildProcess = gulpInput => (
+  gulpInput
     .pipe(errorsLogger())
     // .pipe(compilationLogger())
     .pipe(babel(babelConfig))
-    .pipe(gulp.dest(path.join(pkgLocation, '/dist')))
+    .pipe(gulp.dest((file) => {
+      const dir = path.join('packages', file.relative.replace(/\/src\/.*/, '/dist/'))
+      // eslint-disable-next-line no-param-reassign
+      file.path = file.relative.replace(/.*\/src\//, 'dist/')
+      return dir
+    }))
 )
 
-const watch = pkgLocation => (
-  gulpWatch(
-    getGlobForPackage(pkgLocation),
-    { debounceDelay: 200 },
-    gulp.task('build'),
+const build = () => (
+  // gutil.log(`Building '${chalk.cyan(pkg.name())}'`)
+  buildProcess(
+    gulp.src(srcFiles, { base: 'packages' }),
   )
 )
+
+const watch = () => {
+  const watcher = gulp.watch(srcFiles, { delay: 50 })
+  watcher.on('change', (filePath) => {
+    gutil.log(`Compiling '${chalk.cyan(filePath)}'...`)
+    buildProcess(
+      gulp.src(filePath, { base: 'packages' }),
+    )
+  })
+}
 
 const everyPackage = task => (
   packages.map(pkgLocation => done => task(pkgLocation, done))
@@ -62,16 +78,16 @@ const everyPackage = task => (
 
 gulp.task(
   'clean',
-  gulp.parallel(everyPackage(clean)),
+  clean,
 )
 gulp.task(
   'build',
-  gulp.series('clean', ...everyPackage(build)),
+  gulp.series('clean', build),
 )
 gulp.task(
   'watch',
   gulp.series(
     'build',
-    gulp.parallel(everyPackage(watch)),
+    watch,
   ),
 )
