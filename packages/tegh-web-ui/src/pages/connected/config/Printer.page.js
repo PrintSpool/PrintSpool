dae267cc8182479a711064d5145621a83664daa9
D1plo1d <thatotherdude@gmail.com>
2018-10-27 02:05:13 -0400

    prototyping the printer page

--- packages/tegh-web-ui/src/pages/connected/config/Printer.page.js
+++ packages/tegh-web-ui/src/pages/connected/config/Printer.page.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { compose, withProps } from 'recompose'
 import {
+  withStyles,
   Grid,
+  Divider,
   Typography,
+  List,
+  ListItem,
+  TextField,
+  MenuItem,
 } from '@material-ui/core'
 import Loader from 'react-loader-advanced'
 import gql from 'graphql-tag'
@@ -10,8 +16,6 @@ import gql from 'graphql-tag'
 import withLiveData from '../shared/higherOrderComponents/withLiveData'
 
 import PrinterStatusGraphQL from '../shared/PrinterStatus.graphql'
-
-import Header from '../frame/components/Header'
 
 const CONFIG_SUBSCRIPTION = gql`
   subscription ConfigSubscription($printerID: ID!) {
@@ -29,7 +33,14 @@ const CONFIG_SUBSCRIPTION = gql`
   ${PrinterStatusGraphQL}
 `
 
+const styles = theme => ({
+  title: {
+    paddingTop: theme.spacing.unit * 3,
+  },
+})
+
 const enhance = compose(
+  withStyles(styles, { withTheme: true }),
   withProps(ownProps => ({
     subscription: CONFIG_SUBSCRIPTION,
     variables: {
@@ -42,11 +53,51 @@ const enhance = compose(
   })),
 )
 
-const PrinterConfigPage = ({ config }) => (
+const PrinterConfigPage = ({ classes, config }) => (
   <main>
+    <TextField
+      required
+      label="Name"
+      margin="normal"
+      fullWidth
+    />
+    <TextField
+      required
+      select
+      label="Make and model"
+      margin="normal"
+      fullWidth
+    >
+      {['Lulzbot Mini 1', 'Lulzbot Mini 2'].map(option => (
+        <MenuItem key={option} value={option}>
+        {option}
+        </MenuItem>
+      ))}
+    </TextField>
+    { console.log(classes.title) }
+    <Typography variant="h5" classes={{ root: classes.title }}>
+      Controllers
+    </Typography>
+    <List>
+      <ListItem button>
+        <div>
+          <Typography variant="h6">Serial Controller Board</Typography>
+        </div>
+      </ListItem>
+    </List>
+
+
+    <TextField
+      required
+      label="Name"
+      margin="normal"
+      fullWidth
+    />
 
   </main>
 )
 
-export const Component = PrinterConfigPage
+export const Component = withStyles(styles, { withTheme: true })(
+  PrinterConfigPage,
+)
 export default enhance(PrinterConfigPage)
