b1391c8d624e1deb0c9547cda25d1c3d34d8ea4b
D1plo1d <thatotherdude@gmail.com>
2018-11-27 02:14:29 -0500

    Refactoring signalling server into modern javascript

--- packages/tegh-signalling-server/src/index.js
+++ packages/tegh-signalling-server/src/index.js
@@ -1,20 +1,24 @@
-var app = require('express')()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
+import express from 'express'
+import http from 'http'
+import socketIO from 'socket.io'
 
-io.on('connection', function(socket){
+const app = express()
+const httpServer = http.Server(app)
+const io = socketIO(httpServer)
+
+io.on('connection', (socket) =>  {
   const { fingerprint } = socket.handshake.query
   // console.log('NEW: ', fingerprint)
   socket.join([fingerprint])
   // console.log('a user connected', socket.handshake.query)
-  socket.on('announcement', function(msg){
-    // console.log('message: ', msg)
-    // console.log('TO:  ', msg.to)
-
+  socket.on('announcement', (msg) =>  {
+    // console.log(`ANNOUNCEMENT TO ${msg.to}`)
+    // console.log(`ANNOUNCEMENT TO ${msg.to}:`, msg.payload)
     io.to(msg.to).emit('announcement', msg)
   })
 })
 
-http.listen(3000, function(){
+httpServer.listen(3000, () => {
+  // eslint-disable-next-line no-console
   console.log('listening on *:3000')
 })
