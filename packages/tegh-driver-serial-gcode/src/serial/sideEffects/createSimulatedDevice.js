4ed1325e2922eeba272ff2ad44552b041de98cb6
D1plo1d <thatotherdude@gmail.com>
2018-12-02 01:55:48 -0500

    Removing console.log

--- packages/tegh-driver-serial-gcode/src/serial/sideEffects/createSimulatedDevice.js
+++ packages/tegh-driver-serial-gcode/src/serial/sideEffects/createSimulatedDevice.js
@@ -2,7 +2,6 @@ import fs from 'fs'
 import Promise from 'bluebird'
 
 const createSimulatedDevice = async (controllerConfig) => {
-  console.log(controllerConfig.serialPortID)
   await Promise.promisify(fs.writeFile)(controllerConfig.serialPortID, '')
 }
 
