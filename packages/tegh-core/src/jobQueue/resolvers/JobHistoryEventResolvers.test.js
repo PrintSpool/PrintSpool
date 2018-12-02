9942aff9199f0c4f246c635cb21a554d36a7a14a
D1plo1d <thatotherdude@gmail.com>
2018-12-02 18:45:38 -0500

    Test: Adding JobHistoryEventResolvers and JobQueueResolvers snaphsot tests

---
+++ packages/tegh-core/src/jobQueue/resolvers/JobHistoryEventResolvers.test.js
@@ -0,0 +1,28 @@
+import typeDefs from 'tegh-schema'
+
+import deterministicTestSetup from '../../util/testing/deterministicTestSetup'
+import snapshotTestResolvers from '../../util/testing/snapshotTestResolvers'
+
+import JobHistoryEvent from '../types/JobHistoryEvent'
+import { FINISH_PRINT } from '../types/JobHistoryTypeEnum'
+
+import JobHistoryEventResolvers from './JobHistoryEventResolvers'
+
+describe('JobHistoryEventResolvers', () => {
+  it('presents a consistent API', () => {
+    deterministicTestSetup()
+
+    const event = JobHistoryEvent({
+      type: FINISH_PRINT,
+    })
+
+
+    snapshotTestResolvers({
+      typeDefs,
+      typeName: 'JobHistoryEvent',
+      resolvers: JobHistoryEventResolvers,
+      rootValue: event,
+      contextValue: {},
+    })
+  })
+})
