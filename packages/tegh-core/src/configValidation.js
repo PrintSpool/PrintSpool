fb513b5e98af900a1715cf25059b26604217e60c
D1plo1d <thatotherdude@gmail.com>
2018-12-09 16:40:39 -0500

    Adding a validation that a material is not in use by a toolhead before it is deleted

--- packages/tegh-core/src/configValidation.js
+++ packages/tegh-core/src/configValidation.js
@@ -1,7 +1,22 @@
 import * as yup from 'yup'
+import { TOOLHEAD } from './config/types/components/ComponentTypeEnum'
 
-const configValidation = () => (
-  yup.object().shape({
+const configValidation = () => yup.object()
+  .test(
+    'material-in-use',
+    'materials in use by a toolhead cannot be deleted',
+    (config) => {
+      const materialIDs = config.materials.map(m => m.id)
+      return config.printer.components
+        .filter(c => c.type === TOOLHEAD)
+        .every((component) => {
+          const materialID = component.model.get('materialID')
+          return materialIDs.includes(materialID)
+        })
+    },
+  )
+  .shape({
+    materials: yup.mixed(),
     printer: yup.object().shape({
       components: yup.mixed()
         .test(
@@ -28,6 +43,5 @@ const configValidation = () => (
         ),
     }),
   })
-)
 
 export default configValidation
