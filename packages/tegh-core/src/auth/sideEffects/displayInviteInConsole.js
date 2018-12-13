52fe9c1b3e8db543afa2e8208f29b8d3779c7fb7
D1plo1d <thatotherdude@gmail.com>
2018-12-13 02:29:08 -0500

    80 character lines

--- packages/tegh-core/src/auth/sideEffects/displayInviteInConsole.js
+++ packages/tegh-core/src/auth/sideEffects/displayInviteInConsole.js
@@ -12,19 +12,22 @@ const displayInviteInConsole = ({ hostDatID, invite }) => {
 
   const inviteString = invitePayload.join('')
 
+  const consoleLine = `${'='.repeat(80)}\n`
+
   qrcode.generate(inviteString, { small: true }, (qr) => {
     /* eslint-disable no-console, prefer-template, comma-dangle */
     console.error(
       '\n\n\n'
-      + '==========================================================\n'
+      + consoleLine
       + 'Invite Code\n'
-      + '==========================================================\n\n'
+      + consoleLine
       + `${qr}\n`
       + `${inviteString}\n\n`
       + 'Your almost ready to start 3D Printing! To connect to your printer:\n\n'
       + '1. Install the Beaker Distributed Web Browser from https://beakerbrowser.com/\n'
       + '2, Go to dat://tegh.io and use the Invite Code above.\n\n'
-      + '==========================================================\n\n'
+      + consoleLine
+      + '\n'
     )
     /* eslint-enable */
   })
