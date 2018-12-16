c19673be50b24e52e35e6b72b5b1e5438947c891
D1plo1d <thatotherdude@gmail.com>
2018-12-16 01:09:46 -0500

    Refactoring spoolMacro

--- packages/tegh-core/src/macros/reducers/macrosReducer.js
+++ packages/tegh-core/src/macros/reducers/macrosReducer.js
@@ -1,55 +1,55 @@
-import { loop, Cmd } from 'redux-loop'
-import { Record, Map } from 'immutable'
-
-import { SET_CONFIG } from '../../config/actions/setConfig'
-import { SPOOL_MACRO } from '../../spool/actions/spoolMacro'
-import spoolTask from '../../spool/actions/spoolTask'
-
-import { NORMAL } from '../../spool/types/PriorityEnum'
-
-import getMacroRunFnsByName from '../../pluginManager/selectors/getMacroRunFnsByName'
-
-export const initialState = Record({
-  config: null,
-  macros: Map(),
-})()
-
-const macrosReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case SET_CONFIG: {
-      const nextState = initialState
-        .set('macros', getMacroRunFnsByName(action.payload))
-        .set('config', action.payload.config)
-
-      return nextState
-    }
-    case SPOOL_MACRO: {
-      const {
-        internal,
-        priority,
-        macro,
-        args,
-      } = action.payload
-
-      const macroRunFn = state.macros.get(macro)
-
-      if (macroRunFn == null) {
-        throw new Error(`Macro ${macro} does not exist`)
-      }
-
-      return loop(state, Cmd.action(
-        spoolTask({
-          name: macro,
-          internal,
-          priority: priority || macroRunFn.priority || NORMAL,
-          data: macroRunFn(args, { config: state.config }),
-        }),
-      ))
-    }
-    default: {
-      return state
-    }
-  }
-}
-
-export default macrosReducer
+// import { loop, Cmd } from 'redux-loop'
+// import { Record, Map } from 'immutable'
+//
+// import { SET_CONFIG } from '../../config/actions/setConfig'
+// import { SPOOL_MACRO } from '../../spool/actions/spoolMacro'
+// import spoolTask from '../../spool/actions/spoolTask'
+//
+// import { NORMAL } from '../../spool/types/PriorityEnum'
+//
+// import getMacroRunFnsByName from '../../pluginManager/selectors/getMacroRunFnsByName'
+//
+// export const initialState = Record({
+//   config: null,
+//   macros: Map(),
+// })()
+//
+// const macrosReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case SET_CONFIG: {
+//       const nextState = initialState
+//         .set('macros', getMacroRunFnsByName(action.payload))
+//         .set('config', action.payload.config)
+//
+//       return nextState
+//     }
+//     case SPOOL_MACRO: {
+//       const {
+//         internal,
+//         priority,
+//         macro,
+//         args,
+//       } = action.payload
+//
+//       const macroRunFn = state.macros.get(macro)
+//
+//       if (macroRunFn == null) {
+//         throw new Error(`Macro ${macro} does not exist`)
+//       }
+//
+//       return loop(state, Cmd.action(
+//         spoolTask({
+//           name: macro,
+//           internal,
+//           priority: priority || macroRunFn.priority || NORMAL,
+//           data: macroRunFn(args, { config: state.config }),
+//         }),
+//       ))
+//     }
+//     default: {
+//       return state
+//     }
+//   }
+// }
+//
+// export default macrosReducer
