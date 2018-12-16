ca264c49d7aacb6af2f5e09701ebf3679ea40a11
D1plo1d <thatotherdude@gmail.com>
2018-12-16 02:29:17 -0500

    Adding tegh-raspberry-pi functionality

---
+++ packages/tegh-raspberry-pi/src/actions/gpioError.js
@@ -0,0 +1,8 @@
+import { driverError } from 'tegh-core'
+
+const gpioError = reason => driverError({
+  code: 'tegh-raspberry-pi/gpioError',
+  message: `GPI Error: ${reason}`,
+})
+
+export default gpioError
