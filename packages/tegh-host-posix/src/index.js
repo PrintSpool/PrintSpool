5ac53531d5618507ccbf5e7879b43f87c3d41109
D1plo1d <thatotherdude@gmail.com>
2019-04-10 15:11:43 -0600

    chore: Debugging. Snap user is not being added to the dialout group

--- packages/tegh-host-posix/src/index.js
+++ packages/tegh-host-posix/src/index.js
@@ -1,2 +1,10 @@
 import 'source-map-support/register'
+var exec = require('child_process').exec
+exec("id", function(err, stdout, stderr) {
+  console.log(`ID!!!`, stdout);
+});
+exec("groups", function(err, stdout, stderr) {
+  console.log(`GROUPS!!!!!`, stdout);
+});
+
 import './posixHost'
