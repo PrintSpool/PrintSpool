1daabf07ce615b436d8666d27ab66dea0173746c
D1plo1d <thatotherdude@gmail.com>
2019-05-14 08:23:42 -0600

    feat[Manual Control]: Seperate Home X, Y, Z, XY and Home All buttons

--- packages/tegh-web-ui/src/pages/connected/manualControl/components/home/Home.js
+++ packages/tegh-web-ui/src/pages/connected/manualControl/components/home/Home.js
@@ -4,7 +4,11 @@ import {
   Card,
   CardContent,
   Button,
+  Typography,
 } from '@material-ui/core'
+import {
+  KeyboardArrowDown,
+} from '@material-ui/icons'
 
 import withSpoolMacro from '../../../shared/higherOrderComponents/withSpoolMacro'
 
@@ -24,16 +28,38 @@ const enhance = compose(
 const Home = ({
   doHome,
 }) => (
-  <Card>
-    <CardContent>
-      <div style={{ textAlign: 'right' }}>
-        <Button
-          variant="contained"
-          onClick={() => doHome({ all: true })}
-        >
-          Home
-        </Button>
-      </div>
+  <Card style={{ marginBottom: 16 }}>
+    <CardContent style={{ paddingBottom: 16 }}>
+      <Typography variant="subtitle1">
+        <div style={{ float: 'right', marginTop: -4 }}>
+          <Button
+            onClick={() => doHome(['x'])}
+          >
+            X
+          </Button>
+          <Button
+            onClick={() => doHome(['y'])}
+          >
+            Y
+          </Button>
+          <Button
+            onClick={() => doHome(['z'])}
+          >
+            Z
+          </Button>
+          <Button
+            onClick={() => doHome(['x', 'y'])}
+          >
+            X & Y
+          </Button>
+          <Button
+            onClick={() => doHome({ all: true })}
+          >
+            All
+          </Button>
+        </div>
+        Home
+      </Typography>
     </CardContent>
   </Card>
 )
