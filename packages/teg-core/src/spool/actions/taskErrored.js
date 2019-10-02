97e3339f8ec96a8ad20d9ea927383425494d588d
D1plo1d <thatotherdude@gmail.com>
2019-10-02 13:36:33 -0400

    chore: Deleting legacy spool

--- packages/teg-core/src/spool/actions/taskErrored.js
+++
@@ -1,8 +0,0 @@
-export const TASK_ERRORED = 'teg-core/spool/TASK_ERRORED'
-
-const taskErrored = ({ task }) => ({
-  type: TASK_ERRORED,
-  payload: { task },
-})
-
-export default taskErrored
--- packages/teg-core/src/spool/selectors/getCurrentTask.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from 'reselect'
-
-const getCurrentTask = createSelector(
-  state => state.tasks.get(state.currentTaskID),
-)
-
-export default getCurrentTask
