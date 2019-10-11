a8f60572d4f24e6c8cee576b8407ae64dbcc87f4
D1plo1d <thatotherdude@gmail.com>
2019-10-10 21:11:43 -0400

    fix: build fixes.

--- scripts/build-snap.sh
+++ scripts/build-snap.sh
@@ -3,14 +3,14 @@ set -e
 
 mkdir -p ./dist
 rm -f ./dist/*.snap
-#
-# ./scripts/build-armv7-pkg.sh
-# ./scripts/build-x64-pkg.sh
-#
-# echo "\nBuilding teg-marlin...\n\n"
-# yarn tegmarlin:build:x64
-# yarn tegmarlin:build:armv7
-# echo "\n\nBuilding teg-marlin... [DONE]\n"
+
+./scripts/build-armv7-pkg.sh
+./scripts/build-x64-pkg.sh
+
+echo "\nBuilding teg-marlin...\n\n"
+yarn tegmarlin:build:x64
+yarn tegmarlin:build:armv7
+echo "\n\nBuilding teg-marlin... [DONE]\n"
 
 TEG_VERSION=`node -e "console.log(require('./packages/teg-core/package.json').version);"`;
 
