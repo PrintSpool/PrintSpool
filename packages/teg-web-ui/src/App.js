514f15e2553690797c11aadbda1f55c52ac05131
D1plo1d <thatotherdude@gmail.com>
2020-01-20 13:40:10 -0500

    chore: integrating userProfile server

--- packages/teg-web-ui/src/App.js
+++ packages/teg-web-ui/src/App.js
@@ -13,6 +13,7 @@ import ErrorBoundary from 'react-error-boundary'
 import { SnackbarProvider } from 'notistack'
 import { BrowserRouter } from 'react-router-dom'
 import { Route } from 'react-router'
+import { GraphQL, GraphQLProvider } from 'graphql-react'
 
 import TegApolloProvider from './TegApolloProvider'
 
@@ -27,6 +28,9 @@ import ErrorFallback from './common/ErrorFallback'
 import theme from './theme'
 import './i18n'
 
+// Zero config GraphQL client that manages the cache.
+const graphql = new GraphQL()
+
 const auth0Config = {
   domain: 'thirtybots-dev.eu.auth0.com',
   clientID: 'c0iSV8M7sXIJUkb3Q00XfopoC85SNmbm',
@@ -35,40 +39,42 @@ const auth0Config = {
 const App = () => (
   <CssBaseline>
     <ThemeProvider theme={theme}>
-      <Auth0Provider
-        domain={auth0Config.domain}
-        client_id={auth0Config.clientID}
-        redirect_uri={window.location.origin}
-        // onRedirectCallback={onRedirectCallback}
-      >
-        <SnackbarProvider maxSnack={3}>
-          <UserDataProvider filePath="/teg-user.json">
-            <PrintFilesContext.Provider value={useState()}>
-              <React.Suspense fallback={<Loading fullScreen />}>
-                <BrowserRouter>
-                  <Route
-                    path={[
-                      '/m/:hostID/',
-                      '/q/:hostID/',
-                      '/',
-                    ]}
-                    render={() => (
-                      <ErrorBoundary FallbackComponent={ErrorFallback}>
-                        <TegApolloProvider>
-                          <Routes />
-                        </TegApolloProvider>
-                      </ErrorBoundary>
-                    )}
-                  />
-                </BrowserRouter>
-              </React.Suspense>
-              {
-                // <ReduxSnackbar />
-              }
-            </PrintFilesContext.Provider>
-          </UserDataProvider>
-        </SnackbarProvider>
-      </Auth0Provider>
+      <GraphQLProvider graphql={graphql}>
+        <Auth0Provider
+          domain={auth0Config.domain}
+          client_id={auth0Config.clientID}
+          redirect_uri={window.location.origin}
+          // onRedirectCallback={onRedirectCallback}
+        >
+          <SnackbarProvider maxSnack={3}>
+            <UserDataProvider filePath="/teg-user.json">
+              <PrintFilesContext.Provider value={useState()}>
+                <React.Suspense fallback={<Loading fullScreen />}>
+                  <BrowserRouter>
+                    <Route
+                      path={[
+                        '/m/:hostID/',
+                        '/q/:hostID/',
+                        '/',
+                      ]}
+                      render={() => (
+                        <ErrorBoundary FallbackComponent={ErrorFallback}>
+                          <TegApolloProvider>
+                            <Routes />
+                          </TegApolloProvider>
+                        </ErrorBoundary>
+                      )}
+                    />
+                  </BrowserRouter>
+                </React.Suspense>
+                {
+                  // <ReduxSnackbar />
+                }
+              </PrintFilesContext.Provider>
+            </UserDataProvider>
+          </SnackbarProvider>
+        </Auth0Provider>
+      </GraphQLProvider>
     </ThemeProvider>
   </CssBaseline>
 )
--- packages/teg-web-ui/src/printer/home/Home.js
+++ packages/teg-web-ui/src/printer/home/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { useGraphQL } from 'graphql-react'
 
 import {
   Button,
@@ -18,7 +19,7 @@ import StaticTopNavigation from '../../common/topNavigation/StaticTopNavigation'
 import NavigationAuthLink from '../../common/auth/NavigationAuthLink'
 import PrintButton from '../printButton/PrintButton'
 
-const useUserProfile = (query) => {
+const WithAuth0Token = Child => props => {
   const auth0 = useAuth0()
   const [state, setState] = useState({ loading: true })
 
@@ -26,56 +27,124 @@ const useUserProfile = (query) => {
     if (!auth0.isAuthenticated) return
 
     const fetchData = async () => {
-      const token = auth0.getTokenSilently()
-
-      const headers = new Headers()
-      headers.append('Authorization', `Bearer ${token}`)
-
-      const formData = new FormData()
-      formData.append('query', query)
-
-      const req = new Request('https://app-f49757b3-f48d-4078-8e8c-47b27b8b9d6d.cleverapps.io/graphql', {
-        method: 'POST',
-        mode: 'cors',
-        headers,
-        body: formData,
-      })
-
-      const res = await fetch(req)
-      if (!res.ok) {
-        throw new Error(`HTTP error, status = ${res.status}`)
-      }
-      
-      const json = await res.json()
-
-      if (json.error != null) {
-        throw new Error(`GraphQL error = ${json.error}`)
-      }
-
-      setState({ loading: false, data: json })
+      const auth0Token = await auth0.getTokenSilently()
+      setState({ loading: false, auth0Token })
     }
 
     fetchData()
   }, [auth0.isAuthenticated])
 
-  return state
+  if (loading) {
+    return <div></div>
+  }
+
+  return (
+    <Child auth0Token={state.auth0Token} {...props} />
+  )
 }
 
-const Home = () => {
+// const useUserProfile = (query) => {
+//   const auth0 = useAuth0()
+//   const [state, setState] = useState({ loading: true })
+
+//   useEffect(() => {
+//     if (!auth0.isAuthenticated) return
+
+//     const fetchData = async () => {
+//       // const url = 'https://app-f49757b3-f48d-4078-8e8c-47b27b8b9d6d.cleverapps.io/graphql'
+//       const url = 'http://localhost:8080/graphql'
+
+//       const token = await auth0.getTokenSilently()
+
+//       const headers = new Headers({
+//         Authorization: `Bearer ${token}`,
+//         ContentType: 'application/json',
+//       })
+
+//       const req = new Request(url, {
+//         method: 'POST',
+//         mode: 'cors',
+//         headers,
+//         body: JSON.stringify({
+//           query,
+//         }),
+//       })
+
+//       const res = await fetch(req)
+
+//       const json = await res.json()
+
+//       if (!res.ok) {
+//         console.error(json)
+//         throw new Error(`HTTP error, status = ${res.status}`)
+//       }
+
+//       if (json.error != null) {
+//         throw new Error(`GraphQL error = ${json.error}`)
+//       }
+
+//       console.log(json)
+//       setState({ loading: false, ...json })
+//     }
+
+//     fetchData()
+//   }, [auth0.isAuthenticated])
+
+//   return state
+// }
+
+const userProfileServerFetchOptions = auth0Token => optons => {
+    // const url = 'https://app-f49757b3-f48d-4078-8e8c-47b27b8b9d6d.cleverapps.io/graphql'
+    const url = 'http://localhost:8080/graphql'
+
+    const headers = new Headers({
+      Authorization: `Bearer ${auth0Token}`,
+      ContentType: 'application/json',
+    })
+
+    Object.assign(options, {
+      url,
+      mode: 'cors',
+      headers,
+    })
+}
+
+const Home = ({
+  auth0Token,
+}) => {
   const classes = HomeStyles()
 
-  const { loading, data } = useUserProfile(`
-    query {
-      my {
-        machines {
-          id
-          public_key
-          name
-          slug
-        }
-      }
-    }
-  `)
+  // const { loading, data } = useUserProfile(`
+  //   query {
+  //     my {
+  //       machines {
+  //         id
+  //         publicKey
+  //         name
+  //         slug
+  //       }
+  //     }
+  //   }
+  // `)
+
+  // const { loading, cacheValue = {} } = useGraphQL({
+  //   fetchOptionsOverride: userProfileServerFetchOptions(auth0Token),
+  //   operation: {
+  //     query: `
+  //       {
+  //         my {
+  //           machines {
+  //             id
+  //             publicKey
+  //             name
+  //             slug
+  //           }
+  //         }
+  //       }
+  //     `
+  //   },
+  //   loadOnMount: true,
+  // })
 
   const navActions = ({ buttonClass }) => (
     <>
@@ -131,4 +200,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default WithAuth0Token(Home)
