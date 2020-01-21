4eb6daaffb1593a6a5496a4bd5df87f9c3455ce6
D1plo1d <thatotherdude@gmail.com>
2020-01-21 10:51:51 -0500

    chore: Adding missing files

---
+++ packages/teg-web-ui/src/common/userProfileServer/fetchOptions.js
@@ -0,0 +1,18 @@
+
+const userProfileServerFetchOptions = auth0Token => options => {
+  // const url = 'https://app-f49757b3-f48d-4078-8e8c-47b27b8b9d6d.cleverapps.io/graphql'
+  const url = 'http://localhost:8080/graphql'
+
+  const headers = new Headers({
+    Authorization: `Bearer ${auth0Token}`,
+    ContentType: 'application/json',
+  })
+
+  Object.assign(options, {
+    url,
+    mode: 'cors',
+    headers,
+  })
+}
+
+export default userProfileServerFetchOptions
