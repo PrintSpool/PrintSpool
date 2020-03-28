483fe71065cf9be7678d5ec96cf7680d9ef29834
D1plo1d <thatotherdude@gmail.com>
2020-03-27 20:45:08 -0400

    regression: Fixed graphql schema loading in pkg

--- packages/teg-schema/src/index.js
+++ packages/teg-schema/src/index.js
@@ -1,12 +1,12 @@
 import fs from 'fs'
 
-import index from './index.graphql'
-import auth from './schemaModules/auth.graphql'
-import config from './schemaModules/config.graphql'
-import devices from './schemaModules/devices.graphql'
-import jobQueue from './schemaModules/jobQueue.graphql'
-import machine from './schemaModules/machine.graphql'
-import spool from './schemaModules/spool.graphql'
+import index from './index.graphql.js'
+import auth from './schemaModules/auth.graphql.js'
+import config from './schemaModules/config.graphql.js'
+import devices from './schemaModules/devices.graphql.js'
+import jobQueue from './schemaModules/jobQueue.graphql.js'
+import machine from './schemaModules/machine.graphql.js'
+import spool from './schemaModules/spool.graphql.js'
 
 const tegTypeDefs = [
   index,
