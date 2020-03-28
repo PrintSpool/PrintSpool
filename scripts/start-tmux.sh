4d8d631ab52352c49d2f5be04f932bf49ec7fbe5
D1plo1d <thatotherdude@gmail.com>
2020-03-28 13:19:59 -0400

    chore: Integrated cargo watch and restart on reset with marlin, auth and tmux

--- scripts/start-tmux.sh
+++ scripts/start-tmux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 tmux new-session -d '$SHELL --rcfile <(echo ". ~/.bashrc; cd ./packages/teg-host-posix/ && history -s \"yarn dev\" && yarn dev")'
-tmux split-window -v '$SHELL --rcfile <(echo ". ~/.bashrc; cd ./packages/teg-auth/ && history -s \"cargo run\" && RUST_ENV=development cargo run")'
-tmux split-window -v '$SHELL --rcfile <(echo ". ~/.bashrc; cd ./packages/teg-marlin/ && history -s \"cargo run\" && RUST_ENV=development cargo run")'
-tmux split-window -h '$SHELL --rcfile <(echo ". ~/.bashrc; history -s \"yarn start:web-ui\" && yarn start:web-ui")'
+tmux split-window -v '$SHELL --rcfile <(echo ". ~/.bashrc; cd ./packages/teg-auth/ && history -s \"RUST_ENV=development cargo watch -s \\\"cargo run && touch ./Cargo.toml\\\"\" && RUST_ENV=development cargo watch -s \"cargo run && touch ./Cargo.toml\"")'
+tmux split-window -v '$SHELL --rcfile <(echo ". ~/.bashrc; cd ./packages/teg-marlin/ && history -s \"RUST_ENV=development cargo watch -s \\\"cargo run && touch ./Cargo.toml\\\"\" && RUST_ENV=development cargo watch -s \"cargo run && touch ./Cargo.toml\"")'
+tmux split-window -h '$SHELL --rcfile <(echo ". ~/.bashrc; cd ./packages/teg-web-ui/ && history -s \"yarn start\" && yarn start")'
 tmux new-window 'mutt'
 tmux -2 attach-session -d
