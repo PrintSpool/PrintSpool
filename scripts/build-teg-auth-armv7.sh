0eb33d754f7f02591fb44b6a220567ec9e42b089
D1plo1d <thatotherdude@gmail.com>
2020-04-05 14:49:00 -0400

    fix: Fixed teg-auth armv7 build script

---
+++ scripts/build-teg-auth-armv7.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+# ARCH will be auto-detected as the host if not specified
+#ARCH=i486
+#ARCH=x86_64
+#ARCH=powerpc
+ARCH=arm
+#ARCH=microblaze
+#ARCH=mips
+#ARCH=mipsel
+
+CC_BASE_PREFIX=/opt/cross
+
+# If you use arm, you may need more fine-tuning:
+# arm hardfloat v7
+TRIPLE=arm-linux-musleabihf
+GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv7-a --with-float=hard --with-fpu=vfpv3-d16"
+GCC_CONFFLAGS="--with-arch=armv7-a --with-float=hard --with-fpu=vfpv3-d16"
+
+# arm softfp
+#TRIPLE=arm-linux-musleabi
+#GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv7-a --with-float=softfp"
+#GCC_CONFFLAGS="--with-arch=armv7-a --with-float=softfp"
+
+MAKEFLAGS=-j8
+
+# Enable this to build the bootstrap gcc (thrown away) without optimization, to reduce build time
+# GCC_STAGE1_NOOPT=1
+
+# GCC_BUILTIN_PREREQS=yes
+
+cd ./packages/teg-auth
+
+# CC=arm-linux-gnueabihf-gcc
+
+cargo build --release --target=armv7-unknown-linux-gnueabihf
+
+cp -R ./target/armv7-unknown-linux-gnueabihf/release/teg-auth ../../snap/teg-auth-bin/teg-auth-armv7
