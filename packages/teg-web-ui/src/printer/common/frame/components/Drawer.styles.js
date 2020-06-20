d618cbba258d243ce938e15d9d1cbe92556feda6
d1plo1d <d1plo1d>
2020-06-19 20:13:01 -0400

    fix: Clicking a link in the mobile navigation should close the nav drawer

---
+++ packages/teg-web-ui/src/printer/common/frame/components/Drawer.styles.js
@@ -0,0 +1,53 @@
+import { makeStyles } from '@material-ui/styles'
+
+const drawerWidth = 280
+
+// eslint-disable-next-line
+const useStyles = makeStyles(theme => ({
+  // root: {
+  //   height: '100%'
+  //   width: '100%',
+  //   height: 430,
+  //   zIndex: 1,
+  //   overflow: 'hidden',
+  // },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  fullHeight: {
+    height: '100%',
+  },
+  drawerRoot: {
+    height: '100%',
+  },
+  drawerPaper: {
+    width: 280,
+    height: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: drawerWidth,
+      position: 'inherit',
+      top: 'inherit',
+    },
+  },
+  content: {
+    backgroundColor: theme.palette.background.default,
+    width: '100%',
+    padding: theme.spacing(3),
+    height: 'calc(100% - 56px)',
+    marginTop: 56,
+    [theme.breakpoints.up('sm')]: {
+      height: 'calc(100% - 64px)',
+      marginTop: 64,
+    },
+  },
+  activeLink: {
+    backgroundColor: '#ccc',
+  },
+  drawerContents: {
+    paddingBottom: 0,
+  },
+}), { withTheme: true })
+
+export default useStyles
---
+++ packages/teg-web-ui/src/printer/common/frame/components/Drawer.tsx
@@ -0,0 +1,164 @@
+import React, { useMemo, useCallback } from 'react'
+
+import MaterialUIDrawer from '@material-ui/core/Drawer'
+import Hidden from '@material-ui/core/Hidden'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListSubheader from '@material-ui/core/ListSubheader'
+
+import Inbox from '@material-ui/icons/Inbox'
+import OpenWith from '@material-ui/icons/OpenWith'
+import Code from '@material-ui/icons/Code'
+import Keyboard from '@material-ui/icons/Keyboard'
+import Settings from '@material-ui/icons/Settings'
+import Home from '@material-ui/icons/Home'
+
+import { Link } from 'react-router-dom'
+import gql from 'graphql-tag'
+import useStyles from './Drawer.styles'
+import useRouter from 'use-react-router'
+
+export const DrawerFragment = gql`
+  fragment DrawerFragment on Query {
+    machines {
+      id
+      name
+    }
+  }
+`
+
+const Drawer = ({
+  machineSlug,
+  machines,
+  mobileOpen,
+  onClose,
+  className,
+}) => {
+  const classes = useStyles()
+  const { location } = useRouter()
+
+  console.log('pathname', location.pathname)
+  const DrawerLink = useCallback(({
+    text,
+    href,
+    icon,
+  }) => {
+    console.log('memoized pathname', location.pathname)
+
+    const linkComponent = useMemo(() => React.forwardRef((props, ref) => (
+      <Link to={href} innerRef={ref} {...props} />
+    )), [href])
+
+    return (
+      <ListItem
+        button
+        component={linkComponent as any}
+        onClick={onClose}
+        className={location.pathname === href ? classes.activeLink : ''}
+      >
+        {
+          icon && (
+            <ListItemIcon>
+              {icon}
+            </ListItemIcon>
+          )
+        }
+        <ListItemText primary={text} />
+      </ListItem>
+    )
+  }, [location])
+
+  const drawerContent = () => (
+    <div>
+      <List className={classes.drawerContents}>
+        <Hidden mdUp>
+          <DrawerLink
+            text="Home"
+            icon={<Home />}
+            href="/"
+          />
+        </Hidden>
+        { machines[0] && (
+          <ListSubheader>{machines[0].name}</ListSubheader>
+        )}
+        <DrawerLink
+          text="Printing"
+          icon={<Inbox />}
+          href={`/q/${machineSlug}/`}
+        />
+        {
+          machines.map(machine => (
+            <div
+              key={machine.id}
+            >
+              { machine.length > 1 && (
+                <ListSubheader>
+                  { machine.name }
+                </ListSubheader>
+              )}
+              <DrawerLink
+                text="Maintenance"
+                icon={<OpenWith />}
+                href={`/m/${machineSlug}/${machine.id}/manual-control/`}
+              />
+              <DrawerLink
+                text="Terminal"
+                icon={<Keyboard />}
+                href={`/m/${machineSlug}/${machine.id}/terminal/`}
+              />
+              <DrawerLink
+                text="Config"
+                icon={<Settings />}
+                href={`/m/${machineSlug}/${machine.id}/config/`}
+              />
+            </div>
+          ))
+        }
+        <ListSubheader>Dev Tools</ListSubheader>
+        <DrawerLink
+          text="GraphQL"
+          icon={<Code />}
+          href={`/q/${machineSlug}/graphql-playground/`}
+        />
+      </List>
+    </div>
+  )
+
+  return (
+    <div className={`${classes.fullHeight} ${className}`}>
+      <Hidden mdUp>
+        <MaterialUIDrawer
+          // type="temporary"
+          anchor="left"
+          open={mobileOpen}
+          onClose={onClose}
+          classes={{
+            root: classes.drawerRoot,
+            paper: classes.drawerPaper,
+          }}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+        >
+          { drawerContent() }
+        </MaterialUIDrawer>
+      </Hidden>
+      <Hidden smDown implementation="css">
+        <MaterialUIDrawer
+          variant="persistent"
+          open
+          classes={{
+            root: classes.drawerRoot,
+            paper: classes.drawerPaper,
+          }}
+        >
+          { drawerContent() }
+        </MaterialUIDrawer>
+      </Hidden>
+    </div>
+  )
+}
+
+export default Drawer
