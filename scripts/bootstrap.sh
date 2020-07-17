926c869ad13c6f0058f09af14b149d54d582147a
D1plo1d <thatotherdude@gmail.com>
2020-07-17 08:49:56 -0400

    chore: Removed sqlx from bootstrapping script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-cargo install --git https://github.com/launchbadge/sqlx.git cargo-sqlx
+# cargo install --git https://github.com/launchbadge/sqlx.git cargo-sqlx
 cargo install cargo-watch
 # cargo install cargo-watch cargo-sqlx
 
-cd ./packages/teg-auth/
-sqlx db create
-sqlx migrate run
+# cd ./packages/teg-auth/
+# sqlx db create
+# sqlx migrate run
 
 yarn
 yarn make-dirs
 
-cd ../teg-web-ui/
+cd .packages/teg-web-ui/
 yarn
 
 cd ../teg-host-posix/
