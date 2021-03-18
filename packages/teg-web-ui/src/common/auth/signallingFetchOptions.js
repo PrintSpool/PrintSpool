dc52544470aa7bc1a6b5be48af448b2e20ea217d
D1plo1d <thatotherdude@gmail.com>
2021-03-18 16:46:59 -0400

    chore: Added missed file

---
+++ packages/teg-web-ui/src/common/auth/signallingFetchOptions.js
@@ -0,0 +1,30 @@
+
+const signallingFetchOptions = idToken => (options) => {
+  let url
+
+  // const useDevUserProfileServer = true
+  const useDevUserProfileServer = false
+
+  if (
+    process.env.NODE_ENV === 'production'
+    || useDevUserProfileServer === false
+  ) {
+    // url = 'https://teg-signalling.cleverapps.io/graphql'
+    url = 'https://signalling.onrender.com/graphql'
+  } else {
+    url = 'http://localhost:8080/graphql'
+  }
+
+  const headers = new Headers({
+    Authorization: `Bearer ${idToken}`,
+    'Content-Type': 'application/json',
+  })
+
+  Object.assign(options, {
+    url,
+    mode: 'cors',
+    headers,
+  })
+}
+
+export default signallingFetchOptions
