5512fcd889230d37f73bfce3f29ede9755711644
D1plo1d <thatotherdude@gmail.com>
2021-05-15 16:35:11 -0400

    chore: Updated to tempfile linux persistence branch

--- crates/data_channel/src/saltyrtc_chunk.rs
+++ crates/data_channel/src/saltyrtc_chunk.rs
@@ -153,15 +153,16 @@ impl ChunkDecoder {
                         // Create a *persistable* tmpfile that is automatically cleaned up if
                         // it's file descriptor is dropped via O_TMPFILE
                         use std::io::{ Write, Seek, SeekFrom };
-                        use std::os::unix::fs::OpenOptionsExt as _;
+                        // use std::os::unix::fs::OpenOptionsExt as _;
                         // let mut file: std::fs::File = tempfile::tempfile()?;
                         let tmp_dir = "/var/lib/teg/tmp/";
 
-                        let mut file = std::fs::OpenOptions::new()
-                            .read(true)
-                            .write(true)
-                            .custom_flags(libc::O_TMPFILE) // do not mix with `create_new(true)`
-                            .open(&tmp_dir)?;
+                        // let mut file = std::fs::OpenOptions::new()
+                        //     .read(true)
+                        //     .write(true)
+                        //     .custom_flags(libc::O_TMPFILE) // do not mix with `create_new(true)`
+                        //     .open(&tmp_dir)?;
+                        let mut file = tempfile::tempfile_in(&tmp_dir)?;
 
                         file.write_all(&file_content)?;
                         file.flush()?;
